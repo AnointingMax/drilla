7514e21a5b198654fe5497af67c516f3e1111259
Anointing Maxwell <anointingmax@gmail.com>
2025-02-13 07:14:10 +0100

    Add assets and shadcn components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import './App.css'
 
 function App() {
-
   return (
     <div>
       <h1 className="text-3xl text-red-500 font-bold underline">
---
+++ src/assets/svgs/index.ts
@@ -0,0 +1,33 @@
+import BusIcon from "./bus.svg";
+import CardIcon from "./card.svg";
+import EmailIcon from "./email.svg";
+import ExportIcon from "./export.svg";
+import FacebookIcon from "./facebook.svg";
+import GalleryIcon from "./gallery.svg";
+import InstagramIcon from "./instagram.svg";
+import MinusIcon from "./minus.svg";
+import PhoneIcon from "./phone.svg";
+import PlusIcon from "./plus.svg";
+import SearchIcon from "./search.svg";
+import UserIcon from "./user.svg";
+import XIcon from "./x.svg";
+import YoutubeIcon from "./youtube.svg";
+import MenuIcon from "./menu.svg";
+
+export {
+   BusIcon,
+   CardIcon,
+   EmailIcon,
+   ExportIcon,
+   FacebookIcon,
+   GalleryIcon,
+   InstagramIcon,
+   MinusIcon,
+   PhoneIcon,
+   PlusIcon,
+   SearchIcon,
+   UserIcon,
+   XIcon,
+   YoutubeIcon,
+   MenuIcon
+}
