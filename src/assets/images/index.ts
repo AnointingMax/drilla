292abdf13d616e4dc044fb6f99e637b74cbc0bb4
Anointing Maxwell <anointingmax@gmail.com>
2025-02-14 08:31:05 +0100

    Contact page

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -4,5 +4,6 @@ import Hero from "./hero.png"
 import Display from "./display.png"
 import Image1 from "./image-1.png"
 import Image2 from "./image-2.png"
+import About from "./about.png"
 
-export { Logo, Hero, Display, Image1, Image2, LogoInverted }
+export { Logo, Hero, Display, Image1, Image2, LogoInverted, About }
--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,7 +1,110 @@
+import { About } from "@/assets"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { Button } from "@/components/ui/button"
+import {
+   Form,
+   FormControl,
+   FormField,
+   FormItem,
+   FormLabel,
+   FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Textarea } from "@/components/ui/textarea"
+
+const formSchema = z.object({
+   email: z.string().email().nonempty("This field is required"),
+   phone: z.string().nonempty("This field is required"),
+   message: z.string().nonempty("This field is required"),
+})
 
 const Contact = () => {
+   const form = useForm<z.infer<typeof formSchema>>({
+      resolver: zodResolver(formSchema),
+      defaultValues: {
+         phone: "",
+         message: "",
+         email: "",
+      },
+   })
+
+   function onSubmit(values: z.infer<typeof formSchema>) {
+      console.log(values)
+   }
+
    return (
-      <div>Contact</div>
+      <div className="grid md:grid-cols-2 gap-10 min-h-[70vh] mb-24">
+         <div className="grid">
+            <h1 className="text-dark-text text-2xl font-bold font-Archivo mt-auto">Contact Us</h1>
+            <p className="text-sub-text font-medium text-sm max-w-[250px]">
+               Send us a message, or put a call
+               through we are glad to hear from you
+            </p>
+            <Form {...form}>
+               <form onSubmit={form.handleSubmit(onSubmit)} className="grid gap-4 sm:max-w-[450px] mb-auto">
+                  <FormField
+                     control={form.control}
+                     name="email"
+                     render={({ field }) => (
+                        <FormItem>
+                           <FormLabel>Email</FormLabel>
+                           <FormControl className="bg-[#FBF7F7] border-[#D3D3D4]">
+                              <Input placeholder="@example.com" {...field} />
+                           </FormControl>
+                           <FormMessage />
+                        </FormItem>
+                     )}
+                  />
+                  <FormField
+                     control={form.control}
+                     name="phone"
+                     render={({ field }) => (
+                        <FormItem>
+                           <FormLabel>Email</FormLabel>
+                           <FormControl className="bg-[#FBF7F7] border-[#D3D3D4]">
+                              <Input placeholder="+234" {...field} />
+                           </FormControl>
+                           <FormMessage />
+                        </FormItem>
+                     )}
+                  />
+                  <FormField
+                     control={form.control}
+                     name="message"
+                     render={({ field }) => (
+                        <FormItem>
+                           <FormLabel>Message</FormLabel>
+                           <FormControl className="bg-[#FBF7F7] border-[#D3D3D4]">
+                              <Textarea
+                                 placeholder="Type Message"
+                                 {...field}
+                              />
+                           </FormControl>
+                           <FormMessage />
+                        </FormItem>
+                     )}
+                  />
+                  <Button type="submit" className="h-10">Send Message</Button>
+               </form>
+            </Form>
+         </div>
+         <div className="bg-contain hidden relative bg-no-repeat text-[#F4EEEE] text-sm font-medium md:flex">
+            <img src={About} className="absolute inset-0 w-full h-full -z-1" alt="" />
+            <div className="z-10 gradient h-2/3 flex flex-col mt-auto px-6 pb-8">
+               <p className="mt-auto mb-4">
+                  Thank you for being part of our journey to redefine construction solutions with ease and
+                  efficiency. Your trust not only powers seamless projects but also supports reliability and
+                  quality in every rental. Together, we’re building a better future.</p>
+               <p className="">
+                  Adam Cols
+                  <br />
+                  <span className="text-[12px] font-normal">Founder & CEO</span>
+               </p>
+            </div>
+         </div>
+      </div>
    )
 }
 
